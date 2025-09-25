b859e11a1044024db4e426f5b8346e5b38f39700
Jose Manuel Veliz <ducen29@gmail.com>
2025-09-25 16:10:55 -0400

    fix: Add wildcard to trustedOrigins for improved CORS handling

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -13,6 +13,7 @@ export const auth = betterAuth<BetterAuthOptions>({
   trustedOrigins: [
     process.env.CORS_ORIGIN || "",
     "pagalo://",
+    "pagalo://*",
     "exp://s9pku04-ducen29-8081.exp.direct",
     "exp://192.168.1.102:8081",
   ],
