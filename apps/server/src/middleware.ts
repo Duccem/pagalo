2cdea3bf474c0750d98ac410f74458a589ac4223
Jose Manuel Veliz <ducen29@gmail.com>
2025-09-25 16:27:48 -0400

    fix: Update middleware to log request origin correctly

--- apps/server/src/middleware.ts
+++ apps/server/src/middleware.ts
@@ -1,7 +1,9 @@
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  console.log("Request Origin:", req.headers.get("origin"));
+  console.log("Request Origin:", req.headers.keys());
+  console.log(await cookies());
   const res = NextResponse.next();
 
   res.headers.append("Access-Control-Allow-Credentials", "true");
